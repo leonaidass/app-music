710a48f6c8ec01ad1dd1906484e2ac750efe0c39
leonaidass <leo22esquivel@gmail.com>
2022-02-24 23:08:39 -0300

    app-music-front

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+          
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selectItem:0,
+                drawer:null,
+                items:[
+                    {icon:'fas fa-satellite-dish', text:'Discover'},
+                    {icon:'fas fa-stream', text:'Stream'},
+                    {icon:'fas fa-user', text:'Community'},
+                    {icon:'fas fa-chart-pie', text:'Statistics'},
+                ],
+                collections:[
+                    {icon:'fas fa-heart', text:'Likes', route:'/likes'},
+                    {icon:'fas fa-heart', text:'Likes', route:'/likes'},
+                    {icon:'fas fa-heart', text:'Likes', route:'/likes'},
+                    {icon:'fas fa-heart', text:'Likes', route:'/likes'},
+
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app id="inspire" :style="{background: $vuetify.theme.themes.dark.background}">
+  <Sidebar/>
+
+</v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Sidebar from "../components/Sidebar.vue"
+  export default {
+    name: 'Home',
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    components: {
+      Sidebar
+    },
   }
-}
 </script>
